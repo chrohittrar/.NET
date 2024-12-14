ff906ec7d3b49b2a2b2125de299a3e087cbe7e1a
Rohit Kumar <chrohittrar@gmail.com>
2024-12-14 18:56:37 +0530

    C# Basic : Assignments : Dot Net Module

---
+++ C# Basics/Assignment2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Welcome {name}");
+        }
+    }
+}
---
+++ C# Basics/Assignment3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var name in args)
+            {
+                Console.WriteLine($"Welcome {name}");
+            }
+        }
+    }
+}
